f488de76db6c1d544bbf1bf9411d32eff11ed0bf
Slipsoon <44143461+Slipsoon@users.noreply.github.com>
2019-02-04 13:39:08 +0100

    Add files via upload

--- ToDo/MainWindow.xaml.cs
+++ ToDo/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ToDo.ViewModel;
 
 namespace ToDo
 {
@@ -23,6 +24,10 @@ namespace ToDo
         public MainWindow()
         {
             InitializeComponent();
+            var tasks = new TaskListViewModel();
+            this.DataContext = tasks;
+
+            
         }
 
         private void ButtonExit_Click(object sender, RoutedEventArgs e)
@@ -33,6 +38,40 @@ namespace ToDo
         private void GridTopBar_MouseDown(object sender, MouseButtonEventArgs e)
         {
             DragMove();
+        }
+
+        private void ButtonMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Forms.NotifyIcon notifyIcon = new System.Windows.Forms.NotifyIcon();
+            notifyIcon.Icon = new System.Drawing.Icon("icon2.ico");
+            notifyIcon.Visible = true;
+            notifyIcon.MouseClick += new System.Windows.Forms.MouseEventHandler(notifyIcon_Click);
+            notifyIcon.ShowBalloonTip(500, "ToDo", "Don't forget about your tasks!", System.Windows.Forms.ToolTipIcon.Info);
+            this.Hide();
+
+            System.Windows.Forms.ContextMenu notifyIconContextMenu = new System.Windows.Forms.ContextMenu();
+            notifyIconContextMenu.MenuItems.Add("Show my tasks", new EventHandler(Open));
+            notifyIconContextMenu.MenuItems.Add("Close App", new EventHandler(Close));
+
+            notifyIcon.ContextMenu = notifyIconContextMenu;
+        }
+
+        private void Open(object sender, EventArgs e)
+        {
+            this.Show();
+        }
+
+        private void Close(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void notifyIcon_Click(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            if(e.Button == System.Windows.Forms.MouseButtons.Left)
+            {
+                this.Show();
+            }
         }
     }
 }
